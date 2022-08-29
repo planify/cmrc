fd1be290a2ea319fac1bc4471d3409b9f337ca3d
Travis Haynes <travis@hi5dev.com>
2022-08-28 19:14:17 -0700

    Fixed some compiler warnings.

--- tests/enoent.cpp
+++ tests/enoent.cpp
@@ -5,13 +5,13 @@
 CMRC_DECLARE(enoent);
 
 int main() {
-    auto fs = cmrc::enoent::get_filesystem();
-    try {
-        auto data = fs.open("hello.txt");
-    } catch (std::system_error e) {
-        if (e.code() == std::errc::no_such_file_or_directory) {
-            return 1;
-        }
+  auto fs = cmrc::enoent::get_filesystem();
+  try {
+    [[maybe_unused]] auto data = fs.open("hello.txt");
+  } catch (std::system_error &e) {
+    if (e.code() == std::errc::no_such_file_or_directory) {
+      return 1;
     }
-    return 0;
+  }
+  return 0;
 }
